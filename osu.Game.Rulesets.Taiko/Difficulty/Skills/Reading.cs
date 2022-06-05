8581bec00b2ceee6b03634fc9fc748a908ac59bc
Jay L <jaime@merkids.com>
2022-06-05 22:24:41 +1000

    Reading Skill into osu!taiko

---
+++ osu.Game.Rulesets.Taiko/Difficulty/Skills/Reading.cs
@@ -0,0 +1,62 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Game.Rulesets.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Difficulty.Skills;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Taiko.Difficulty.Preprocessing;
+
+namespace osu.Game.Rulesets.Taiko.Difficulty.Skills
+{
+    public class Reading : StrainDecaySkill
+    {
+        protected override double SkillMultiplier => 1;
+        protected override double StrainDecayBase => 0.4;
+        private const double highSvMultiplier = 1;
+        private const double lowSvMultiplier = 1;
+
+        public Reading(Mod[] mods)
+            : base(mods)
+        {
+        }
+
+        protected override double StrainValueOf(DifficultyHitObject current)
+        {
+            TaikoDifficultyHitObject taikoDifficultyHitObject = (TaikoDifficultyHitObject)current;
+            return svBonus(taikoDifficultyHitObject);
+        }
+
+        private double svBonus(TaikoDifficultyHitObject current)
+        {
+            // High SV Variables
+            const double highSvUpperBound = 640;
+            const double highSvLowerBound = 480;
+            const double highSvCenter = (highSvUpperBound + highSvLowerBound) / 2;
+            const double highSvWidth = highSvUpperBound - highSvLowerBound;
+
+            // Low SV Variables
+            const double lowSvDeltaTimeCenter = 200;
+            const double lowSvDeltaTimeWidth = 300;
+            // Maximum center for low sv (for high density)
+            const double lowSvCenterUpperBound = 200;
+            // Minimum center for low sv (for low density)
+            const double lowSvCenterLowerBound = 100;
+            const double lowSvWidth = 160;
+
+            // Calculate low sv center, considering density
+            double lowSvCenter = lowSvCenterUpperBound - (lowSvCenterUpperBound - lowSvCenterLowerBound) * this.sigmoid(current.DeltaTime, lowSvDeltaTimeCenter, lowSvDeltaTimeWidth);
+
+            double highSvBonus = sigmoid(current.EffectiveBPM, highSvCenter, highSvWidth);
+            double lowSvBonus = 1 - sigmoid(current.EffectiveBPM, lowSvCenter, lowSvWidth);
+
+            return highSvMultiplier * highSvBonus + lowSvMultiplier * lowSvBonus;
+        }
+
+        private double sigmoid(double value, double center, double width)
+        {
+            width /= 10;
+            return 1 / (1 + Math.Exp(-(value - center) / width));
+        }
+    }
+}
