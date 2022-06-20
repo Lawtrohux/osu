c73359a5674fd234a4e6991429badbdebc12c7b2
Jay L <jaime@merkids.com>
2022-06-20 21:27:04 +1000

    copyright

--- osu.Game.Rulesets.Taiko/Difficulty/Evaluators/StaminaEvaluator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Evaluators/StaminaEvaluator.cs
@@ -6,6 +6,11 @@ using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Taiko.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Taiko.Objects;
 
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+#nullable disable
+
 namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
 {
     public static class StaminaEvaluator
