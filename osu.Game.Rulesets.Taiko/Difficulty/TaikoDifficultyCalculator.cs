c5e884232c25b2dcd6a3714d2c0467601bbbc296
Jay Lawton <jaime@merkids.com>
2024-12-21 19:42:14 +1000

    bump rhythm multiplier

--- osu.Game.Rulesets.Taiko/Difficulty/TaikoDifficultyCalculator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/TaikoDifficultyCalculator.cs
@@ -21,7 +21,7 @@ namespace osu.Game.Rulesets.Taiko.Difficulty
     public class TaikoDifficultyCalculator : DifficultyCalculator
     {
         private const double difficulty_multiplier = 0.084375;
-        private const double rhythm_skill_multiplier = 0.2 * difficulty_multiplier;
+        private const double rhythm_skill_multiplier = 0.300 * difficulty_multiplier;
         private const double colour_skill_multiplier = 0.375 * difficulty_multiplier;
         private const double stamina_skill_multiplier = 0.375 * difficulty_multiplier;
 
